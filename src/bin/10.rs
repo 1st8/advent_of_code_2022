239f967a44a48e283f99bb2165603cd78d53e3cc
Christoph Geschwind <christoph.geschwind@mixxt.de>
2022-12-10 14:21:37 +0100

    Day 10 part1

---
+++ src/bin/10.rs
@@ -0,0 +1,63 @@
+pub fn part_one(input: &str) -> Option<i32> {
+    let values = register_iter(input).enumerate().filter_map(|(i, x)| {
+        if i % 40 == 19 {
+            Some((i + 1) as i32 * x)
+        } else {
+            None
+        }
+    });
+
+    Some(values.sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 10);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+fn register_iter(input: &str) -> impl Iterator<Item = i32> + '_ {
+    let mut x = 1;
+    input.lines().flat_map(move |line| {
+        let result;
+        match &line.split_whitespace().collect::<Vec<&str>>()[..] {
+            ["noop"] => result = vec![x],
+            ["addx", n] => {
+                result = vec![x, x];
+                x += n.parse::<i32>().unwrap()
+            }
+            _ => panic!(),
+        };
+
+        result
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_register_iter() {
+        assert_eq!(
+            register_iter("addx 15\nnoop").collect::<Vec<_>>(),
+            vec![1, 1, 16]
+        );
+    }
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(part_one(&input), Some(13140));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(part_two(&input), None);
+    }
+}
