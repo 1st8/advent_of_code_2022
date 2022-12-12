71e6c983b4d79e8e517fa2e3641f8967c9579029
Christoph Geschwind <christoph.geschwind@mixxt.de>
2022-12-12 16:38:00 +0100

    Day 12 part2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -9,7 +9,17 @@ impl Pos {
         (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as u32
     }
 
-    fn successors(&self, heightmap: &HashMap<Pos, char>) -> Vec<(Pos, u32)> {
+    // fn apply_to_3<F>(f: F) -> i32 where
+    // // The closure takes an `i32` and returns an `i32`.
+    // F: Fn(i32) -> i32 {
+
+    // f(3)
+    // }
+
+    fn successors<F>(&self, heightmap: &HashMap<Pos, char>, check: F) -> Vec<(Pos, u32)>
+    where
+        F: Fn(i32) -> bool,
+    {
         let current_height = heightmap.get(self).unwrap();
         let mut successors = vec![];
         maybe_add(
@@ -22,20 +32,24 @@ impl Pos {
             &mut successors,
             heightmap,
             current_height,
+            check,
         );
         successors
     }
 }
 
-fn maybe_add(
+fn maybe_add<F>(
     positions: Vec<Pos>,
     acc: &mut Vec<(Pos, u32)>,
     heightmap: &HashMap<Pos, char>,
     current_height: &char,
-) {
+    check: F,
+) where
+    F: Fn(i32) -> bool,
+{
     for pos in positions {
         if let Some(height) = heightmap.get(&pos) {
-            if (*height as i32) - (*current_height as i32) <= 1 {
+            if check((*height as i32) - (*current_height as i32)) {
                 acc.push((pos, 1));
             }
         }
@@ -52,7 +66,7 @@ fn search(search: char) -> impl for<'r, 's> Fn((&'r Pos, &'s char)) -> std::opti
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse(input: &str) -> (Pos, Pos, HashMap<Pos, char>) {
     let mut heightmap: HashMap<Pos, char> = input
         .lines()
         .enumerate()
@@ -68,9 +82,15 @@ pub fn part_one(input: &str) -> Option<u32> {
     heightmap.insert(start.clone(), 'a');
     heightmap.insert(goal.clone(), 'z');
 
+    (start, goal, heightmap)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (start, goal, heightmap) = parse(input);
+
     let (_, count) = astar(
         &start,
-        |p| p.successors(&heightmap),
+        |p| p.successors(&heightmap, |height_diff| height_diff <= 1),
         |p| p.distance(&goal) / 3,
         |p| *p == goal,
     )
@@ -80,7 +100,17 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, goal, heightmap) = parse(input);
+
+    let (_, count) = astar(
+        &goal,
+        |p| p.successors(&heightmap, |height_diff| height_diff >= -1),
+        |p| p.distance(&goal) / 3,
+        |p| *heightmap.get(p).unwrap() == 'a',
+    )
+    .unwrap();
+
+    Some(count)
 }
 
 fn main() {
@@ -102,6 +132,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 12);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(29));
     }
 }
